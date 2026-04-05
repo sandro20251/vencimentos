073398b457f27bf2b83576d72b9d0ba792cb6591
Sandro <sandrocoelho2023a@gmail.com>
2026-04-05 16:45:57 -0300

    estilizando pagina de registro de usuario

--- controller/userController.js
+++ controller/userController.js
@@ -4,4 +4,12 @@ module.exports = class userController {
     static inicio(req, res) {
         res.render('vencimentos/inicio');
     }
+
+    static abrirRegistro(req, res) {
+        res.render('usuarios/registro');
+    }
+
+    static abrirLogin(req, res) {
+        res.render('usuarios/login');
+    }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const userController = require('../controller/userController');
 
 router.get('/', userController.inicio);
+router.get('/abrirRegistro', userController.abrirRegistro);
+router.get('/abrirLogin', userController.abrirLogin);
 
 module.exports = router;
